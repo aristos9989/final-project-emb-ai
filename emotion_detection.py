52b7d06abe7e4382e6b5bf81a4d2b00e648bc8f5
Aristeidis Kartsioukas <aristos9989@gmail.com>
2025-01-24 07:41:03 -0500

    Complete Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detection(text_to_analyze):
@@ -5,4 +6,19 @@ def emotion_detection(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, json=myobj, headers=header)
-    return response.text
+    formatted_text = json.loads(response.text)
+    emotions_dict = formatted_text["emotionPredictions"][0]["emotion"]
+    anger_score = emotions_dict["anger"]
+    disgust_score = emotions_dict["disgust"]
+    fear_score = emotions_dict["fear"]
+    joy_score = emotions_dict["joy"]
+    sadness_score = emotions_dict["sadness"]
+    dominant_emotion = max(emotions_dict, key=emotions_dict.get)
+    return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+    }
